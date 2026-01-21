e093d4e617ddc56b442e376acc75b285557a2726
manmayb <157454794+manmayb@users.noreply.github.com>
2026-01-21 11:15:31 +0530

    Create App.js for the scientific calculator

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Advanced Scientific Calculator</h1>
+            {/* Include your calculator components and logic here */}
+        </div>
+    );
+};
+
+export default App;
