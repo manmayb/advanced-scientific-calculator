fff2f642da8862835ea3ef87ecb88aefca8d5c8f
manmayb <157454794+manmayb@users.noreply.github.com>
2026-01-21 11:15:53 +0530

    Create Display component for showing current input and results

---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Display = ({ value }) => {
+    return (
+        <div className="display">
+            <input type="text" value={value} readOnly />
+        </div>
+    );
+};
+
+export default Display;
